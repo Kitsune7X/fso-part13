76d51153939b482ca14c62ab1757253e2c3b82cf
Kitsune7X <ryuizanagi@proton.me>
2026-02-01 20:24:33 +0900

    add depedencies

---
+++ commands.sql
@@ -0,0 +1,17 @@
+-- Create a new table 'blogs' with a primary key and columns
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    author TEXT,
+    url TEXT NOT NULL,
+    title TEXT NOT NULL,
+    likes INTEGER DEFAULT 0
+);
+
+-- Insert data into 'blogs'
+INSERT INTO blogs (author, url, title)
+VALUES ('Jeff Hardy', 'https://www.youtube.com/watch?v=cquQmv0OD08', 'No more words');
+
+INSERT INTO blogs (author, url, title)
+VALUES ('CM Punk', 'https://www.youtube.com/watch?v=DMdqsS0a9tY', 'This Fire Burns');
+
+SELECT * FROM blogs;
