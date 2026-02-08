d0d3d84aa191059296368f580d7c2d1f566213f4
Kitsune7X <ryuizanagi@proton.me>
2026-02-08 18:44:58 +0900

    scaffold user route handling

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,3 +1,7 @@
 // TODO: Add route handling for users
 // Add bcrypt and stuffs ?
 // First initialize user table
+import express from 'express';
+import User from '../models/user.js';
+
+// User creation handling route
---
+++ src/models/user.ts
@@ -0,0 +1,34 @@
+import { sequelize } from '../utils/db.js';
+import { DataTypes, Model } from 'sequelize';
+
+class User extends Model {
+	declare id: number;
+	declare username: string;
+	declare name: string;
+}
+
+User.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			unique: true,
+			primaryKey: true,
+		},
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		underscored: true,
+		modelName: 'user',
+	},
+);
+
+export default User;
