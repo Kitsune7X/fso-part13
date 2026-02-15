cca0d1b5175bb07519266d3b9bd75a470359e23b
Kitsune7X <ryuizanagi@proton.me>
2026-02-15 17:06:21 +0900

    adjust authentication

--- src/controllers/logout.ts
+++ src/controllers/logout.ts
@@ -1,7 +1,10 @@
 // TODO: Write handle logout route
 import express from 'express';
 import type { Request, Response } from 'express';
+import { isAuthenticated, tokenExtractor } from '../utils/middleware.js';
+import models from '../models/index.js';
 
 const router = express.Router();
+const { User } = models;
 
-router.delete('/', async (req, res) => {});
+router.delete('/', tokenExtractor, isAuthenticated, async (req: Request, res: Response) => {});
--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -5,8 +5,9 @@ import type { DecodedToken } from '../types/types.js';
 import jwt from 'jsonwebtoken';
 import * as z from 'zod';
 import config from './config.js';
+import { Op } from 'sequelize';
 
-const { User, Blog } = models;
+const { User, Blog, Session } = models;
 const { SECRET } = config;
 
 export const blogFinder = async (req: Request, res: Response, next: NextFunction) => {
@@ -17,12 +18,17 @@ export const blogFinder = async (req: Request, res: Response, next: NextFunction
   next();
 };
 
+const TOKEN_START = 7;
+
 export const tokenExtractor = (req: Request, res: Response, next: NextFunction) => {
   const authorization = req.get('authorization');
 
   if (authorization?.toLowerCase().startsWith('bearer ')) {
     try {
-      res.locals.decodedToken = jwt.verify(authorization.substring(7), z.string().parse(SECRET)) as DecodedToken;
+      res.locals.decodedToken = jwt.verify(
+        authorization.substring(TOKEN_START),
+        z.string().parse(SECRET),
+      ) as DecodedToken;
     } catch {
       return res.status(401).json({ error: 'Token invalid' });
     }
@@ -32,7 +38,26 @@ export const tokenExtractor = (req: Request, res: Response, next: NextFunction) 
   return next();
 };
 
-export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  if (req.session.user) next();
-  else res.status(401).json({ error: 'Unauthorized' });
+export const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
+  // Get extract user from token and make sure that expired token cannot be used to authenticate
+  const authorization = req.get('authorization');
+
+  if (authorization?.toLowerCase().startsWith('bearer') && req.session.user) {
+    try {
+      const token = z.string().parse(authorization.substring(TOKEN_START));
+      const userId = req.session.user.id;
+
+      const activeSession = await Session.findOne({
+        where: { [Op.and]: [{ userId, token }] },
+      });
+
+      if (!activeSession?.isLoggedIn) res.status(401).json({ error: 'Unauthorized' });
+
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  } else {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
 };
